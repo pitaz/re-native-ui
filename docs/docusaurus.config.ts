b79ec34c7b2d1e3c94e4359bcaae189c439d91df
pitaz <peter.odekwo@gmail.com>
2025-07-26 18:58:04 +0200

    update base url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: "https://pitaz.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/react-native-ui-library/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   organizationName: "pitaz", // Usually your GitHub org/user name.
